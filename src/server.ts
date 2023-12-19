857c468d6307c41edf27b93c531059fa0f75f92a
lehannK <leandro_hollies@hotmail.com>
2023-12-19 09:13:45 -0300

    adminJS config

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,14 @@
 
 import express from "express";
 import { sequelize } from "./database";
+import { adminJs, adminJsRouter } from "./adminjs";
 
 const app = express();
+
+// método para indicar ao express o diretório 'public', onde serão servidos os arquivos estáticos da página
+app.use(express.static("public"));
+
+app.use(adminJs.options.rootPath, adminJsRouter);
 
 const PORT = process.env.PORT || 3000;
 
