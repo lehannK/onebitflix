cdd8820d5a37f6bbc6ca45832009b744fd33c050
lehannK <leandro_hollies@hotmail.com>
2023-12-19 11:58:30 -0300

    seeders episodes

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,8 +2,12 @@
 
 import { Category } from "./Category";
 import { Course } from "./Course";
+import { Episode } from "./Episode";
 
 Category.hasMany(Course);
 Course.belongsTo(Category);
 
-export { Category, Course };
+Course.hasMany(Episode);
+Episode.belongsTo(Course);
+
+export { Category, Course, Episode };
