937a3daf5b13467238064bae32f8203e112c9b0d
lehannK <leandro_hollies@hotmail.com>
2023-12-26 11:15:26 -0300

    get favorite courses by userId

--- src/services/favoriteService.ts
+++ src/services/favoriteService.ts
@@ -1,11 +1,33 @@
 import { Favorite } from "../models";
 
 export const favoriteService = {
+  // POST /favorites
   create: async (userId: number, courseId: number) => {
     const favorite = Favorite.create({
       courseId,
       userId,
     });
     return favorite;
+  },
+
+  // GET /favorites
+  findByUserId: async (userId: number) => {
+    const favorites = await Favorite.findAll({
+      attributes: [["user_id", "userId"]],
+      where: { userId },
+      include: {
+        association: "Course",
+        attributes: [
+          "id",
+          "name",
+          "synopsis",
+          ["thumbnail_url", "thumbnailUrl"],
+        ],
+      },
+    });
+    return {
+      userId,
+      courses: favorites.map((favorite) => favorite.Course),
+    };
   },
 };
